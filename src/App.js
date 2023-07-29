dfedda63279742e27b641c6258379af5d07d3f84
Salman Tyagi <salmantyagi15@gmail.com>
2023-07-29 16:14:55 +0530

    implemented: how to add list items in component

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Pant", quantity: 1, packed: true },
+];
+
+export default function App() {
+  return (
+    <div className="app ">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🛄</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li style={item.packed ? { textDecoration: "line-through" } : {}}>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items in your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
